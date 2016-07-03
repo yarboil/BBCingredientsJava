244c47750ba0eccf0256611bb2c99f318dbde8d2
Lionel Y <lyarboi@gmail.com>
2016-07-03 23:31:55 +0100

    added new setup for mobile browser setup

--- src/main/java/com/bbcingredients/helper/DriverFactory.java
+++ src/main/java/com/bbcingredients/helper/DriverFactory.java
@@ -7,6 +7,7 @@ import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -14,28 +15,26 @@ import java.net.URL;
 public class DriverFactory {
 
     protected static WebDriver driver;
-    protected static AndroidDriver mobileDriver;
+    protected static WebDriver mobileDriver;
+    protected static AppiumSetup appiumSetup;
 
     public DriverFactory(){
-        initialize();
-    }
-
-    public void initialize(){
-        if(driver == null){
+        if(getDriver() == null){
             createNewDriverInstance();
         }
     }
 
-    private void createNewDriverInstance(){
+    private static void createNewDriverInstance(){
         String browser = new PropertyReader().readProperty("browser");
-        if(browser.equals("firefox")){
+        if(browser.equalsIgnoreCase("firefox")){
             driver = new FirefoxDriver();
-        }else if(browser.equals("chrome")){
+        }else if(browser.equalsIgnoreCase("chrome")){
             driver = new ChromeDriver();
-        }else if(browser.equals("mobile")){
-            AppiumSetup appiumSetup = new AppiumSetup();
+        }else if(browser.equalsIgnoreCase("mobile")){
+            appiumSetup = new AppiumSetup();
             try {
-                mobileDriver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), appiumSetup.getCapabilities());
+                driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), appiumSetup.getCapabilities());
+                mobileDriver = driver;
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
@@ -49,8 +48,13 @@ public class DriverFactory {
         return driver;
     }
 
-    public void destoryDriver(){
+    public WebDriver getMobileDriver(){ return mobileDriver; }
+
+    public void destroyDriver(){
         driver.quit();
         driver = null;
+      if(mobileDriver != null){
+          appiumSetup.stopService();
+      }
     }
 }
